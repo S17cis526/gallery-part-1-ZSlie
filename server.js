34692f7261fe0d4ed28ddedda41e7a8bc0062711
Zachary Sliefert <sliefert@WIN2.CS.KSU.EDU>
2017-01-31 13:59:46 -0600

    Fixed issues with the serveImage function and added ways for /gallery to be accessed.

--- server.js
+++ server.js
@@ -5,46 +5,65 @@
  * This file defines the server for a
  * simple photo gallery web app.
  */
- 
+
 var http = require('http'); /* loads the http library; JS strings don't care about ' or " */
 var fs = require('fs');
 var port = 3000;
-var chess = fs.readFileSync('images/chess.jpg'); /* these options cashe -- but we don't usually want to do this */
-var chess = fs.readFileSync('images/fern.jpg');
+//var chess = fs.readFileSync('images/chess.jpg'); /* these options cashe -- but we don't usually want to do this */
+//var chess = fs.readFileSync('images/fern.jpg');
 
 function serveImage(filename, req, res)
-{	
-	var body = fs.readFileSync('images/' + filename, function(err, body) {
-		if (err) 
+{
+	fs.readFile('images/' + filename, function(err, body) {
+		if (err)
 		{
 			console.error(err);
 			res.statusCode = 500;
 			res.statusMessage = "whoops";
 			res.end("Silly me");
 			return;
-		}	
+		}
 		res.setHeader("Content-Type", "images/jpeg"); /* lets the server know what type of content to expect */
 		res.end(body);
 	});
 }
 
 var server = http.createServer(function (req, res) {
-	
+
 	switch (req.url)
 	{
-		case "/chess":
+    case '/gallery':
+      var html = '<!doctype html>';
+          html += '<head><title>Gallery</title></head>';
+          html += '<body>';
+          html += ' <h1>Gallery</h1>'
+          html += ' <image src="ace.jpg" alt="a fishing ace at work">'
+          html += ' <h1>Hello.</h1> Time is " + Date.now()';
+          html += '</body>';
+      res.setHeader('Content-Type', 'text/html');
+      res.end(html);
+      break;
+    case '/ace':
+    case '/ace.jpg':
+    case '/ace.jpeg':
+      serveImage('ace.jpg', req, res);
+      break;
+    case '/chess.jpg':
+		case '/chess':
+    case '/chess.jpeg':
 			serveImage('chess.jpg', req, res);
 			break;
-		case "/fern":
-		case "/fern/":
-		case "/fern.jpg":
-		case "/fern.jpeg":
+		case '/fern':
+		case '/fern/':
+		case '/fern.jpg':
+		case '/fern.jpeg':
 			serveImage('fern.jpg', req, res);
 			break;
 		default:
 			res.statusCode = 404;
 			res.statusMessage = "Not Found";
-			res.end();
+			res.end("NO");
+      break;
 	}
 }); /* createServer Takes function to handle requests */
 
