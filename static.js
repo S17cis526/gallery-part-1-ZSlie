3c3ea113b2c16bb7881badb6748301a59757f075
Zachary Sliefert <sliefert@WIN2.CS.KSU.EDU>
2017-02-23 14:21:01 -0600

    Added static.js

---
+++ static.js
@@ -0,0 +1,59 @@
+/**@module static
+ * loads and serves static files
+ */
+
+module.exports = {
+    loadDir: loadDir
+}
+
+var files = {};
+var fs = require('fs');
+
+function loadDir(directory){
+    var items = fs.readdirSync(directory);
+    items.forEach(function(item) {
+        var path = directory + '/' + item;
+        var stats = fs.statSync(path);
+        if(stats.isFile()) {
+            var parts = path.split('.');
+            var extension = parts[parts.length - 1];
+            var type = 'application/octet-stream';
+            switch(extension) {
+                case 'css':
+                    type = 'text/css';
+                    break;
+                case 'js':
+                    type = 'text/javascript';
+                    break;
+                case 'jpeg':
+                case 'jpg':
+                    type = 'image/jpeg';
+                    break;
+                case 'gif':
+                case 'png':
+                case 'bmp':
+                case 'tiff':
+                case 'svg':
+                    type = 'image' + extension;
+                    break;
+            }
+            files[path] = {
+                contentType: type,
+                data: fs.readFileSync(path)
+            };
+        }
+        if(stats.isDirectory()) { //Make sure there are no symbolic links in the directory structure!
+            loadDir(path);
+        }
+    });
+}
+
+function isCached(path) {
+    return files[path] != undefined;
+}
+
+function serve(path, req, res) {
+    res.satusCode = 200;
+    res.setHeader('Content-Type', files[path].contentType);
+    res.end(files[path].data);
+}
